186ded71da43583ef8f3662671dc31c24059e57a
CPoblet <cpobleteca@ing.ucsc.cl>
2022-12-12 07:19:57 -0300

    mejor fitness

--- main.py
+++ main.py
@@ -36,15 +36,33 @@ def generate_fitness(price, weight):
         fitness[i] = price[i]/weight[i]
     return fitness
 
+def test_fitness(price, weight, solution):
+    fitness = np.zeros_like(price, dtype=float)
+    norm_p = np.full_like(price, price)
+    norm_p = norm_p/(np.amax(price)+1)
+    carga = evaluation(solution, price, weight)
+    for i in range(len(solution)):
+        carga_i = evaluation(solution, price, weight)
+        if solution[i] == 1:
+            solution[i] = 0
+            fitness[i] = carga_i[1] + norm_p[i]
+            solution[i] = 1
+        else:
+            solution[i] = 1
+            fitness[i] = carga_i[0] + norm_p[i]
+            solution[i] = 0
+    return fitness
+
+
 def sort_fitness(fitness):
     fitness_sorted = np.zeros((2, len(fitness)))
-    max = np.amax(fitness)
+    min = np.amin(fitness)
     for i in range(fitness_sorted.shape[1]):
-        min = np.amin(fitness)
-        index = np.where(fitness == min)
-        fitness[index] = max+1
+        max = np.amax(fitness)
+        index = np.where(fitness == max)
+        fitness[index] = min-1
         fitness_sorted[0][i] = index[0]
-        fitness_sorted[1][i] = min
+        fitness_sorted[1][i] = max
     return fitness_sorted
 
 def evaluation(solution, price, weight):
@@ -82,21 +100,30 @@ def main(argv):
     eva_best_solution = evaluation(best_solution, data[0], data[1])
     #print(solution)
 
+    if eva_best_solution[1] > 1000:
+        best_solution = np.zeros(n, dtype=int)
+        eva_best_solution = evaluation(best_solution, data[0], data[1])
+
     fitness = generate_fitness(data[0], data[1])
     #print(fitness)
-    fitness_sorted = sort_fitness(fitness)
-    #print(fitness_sorted)
 
-    for i in range(iter):
-        solution = best_solution
+    #print(fitness_sorted)
+    count = -1
+    while eva_best_solution[0] != 8373:
+        count += 1
+        fitness = test_fitness(data[0], data[1], best_solution)
+        fitness_sorted = sort_fitness(fitness)
+        solution = np.empty_like(best_solution)
+        solution[:] = best_solution
         r = np.random.rand()
         j = select_roulette(roulette)
         x = int(fitness_sorted[0][j])
         solution[x] = np.absolute(solution[x]-1)
         eva_solution = evaluation(solution, data[0], data[1])
-        if eva_solution[0] >= eva_best_solution[0] and eva_solution[1] <= eva_best_solution[1]:
+        if eva_solution[1] < 1000:
             best_solution = solution
             eva_best_solution = eva_solution
+    print(count)
     print(best_solution)
     print(eva_best_solution)
 
